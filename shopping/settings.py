c72aa9110d3c71beef4bedc5b7eb0f4ae48bf78c
SithabileM <sithabilemonde9@gmail.com>
2025-05-09 16:35:56 +0200

    add hosts fix

--- shopping/settings.py
+++ shopping/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!hbbbaa6u89obkith=rkvc%0s)x*e$*yog5h@92bh%r$!pdmce
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
