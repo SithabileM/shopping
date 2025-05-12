8cbc9b0c18e421d427c41cf0097ae7821976638a
SithabileM <sithabilemonde9@gmail.com>
2025-05-12 16:46:45 +0200

    fix yaml file

--- shopping/settings.py
+++ shopping/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!hbbbaa6u89obkith=rkvc%0s)x*e$*yog5h@92bh%r$!pdmce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG","False")=="True"
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
 
