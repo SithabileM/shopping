87377349a5f5c9c3000dc1106f3e0e9c291b996f
SithabileM <sithabilemonde9@gmail.com>
2025-05-16 03:29:43 +0200

    remove media root and media url

--- shopping/settings.py
+++ shopping/settings.py
@@ -136,9 +136,11 @@ STATIC_URL = 'static/'
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'monde/static')
+"""
 if DEBUG:
     MEDIA_URL='/media/'
     MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
