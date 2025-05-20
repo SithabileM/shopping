2538b9f459de604ac8c437451f8b5f62c5e22f29
SithabileM <sithabilemonde9@gmail.com>
2025-05-20 05:23:17 +0200

    fix yaml error

--- shopping/settings.py
+++ shopping/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,6 +143,7 @@ if DEBUG:
     MEDIA_URL='/media/'
     MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 """
+load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")  # Use service role for server-side ops
 SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "media")
