c6a28d90f1c1c781f3b53963b739844f773ca7d2
SithabileM <sithabilemonde9@gmail.com>
2025-05-21 10:54:39 +0200

    update supabase utils

--- monde/supabase_utils.py
+++ monde/supabase_utils.py
@@ -4,17 +4,26 @@ from uuid import uuid4
 
 supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
+import mimetypes
+
 def upload_image_to_supabase(file, folder="uploads"):
     filename = f"{folder}/{uuid4().hex}_{file.name}"
     file_content = file.read()
 
+    content_type = file.content_type
+    if not content_type:
+        content_type, _ = mimetypes.guess_type(file.name)
+        if not content_type:
+            content_type = "application/octet-stream"
+
     res = supabase.storage.from_(settings.SUPABASE_BUCKET).upload(
         path=filename,
         file=file_content,
-        file_options={"content-type": file.content_type},
+        file_options={"content-type": content_type}
     )
-    
-    if res.get('error'):
+
+    if res.get("error"):
         raise Exception(f"Upload failed: {res['error']['message']}")
-    
+
     return supabase.storage.from_(settings.SUPABASE_BUCKET).get_public_url(filename)
+
