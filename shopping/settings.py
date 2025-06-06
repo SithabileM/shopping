fd0a03b7e0518d01e9f8cce7c6d9995da216b08c
SithabileM <sithabilemonde9@gmail.com>
2025-06-06 16:06:18 +0200

    trigger deployment

--- shopping/settings.py
+++ shopping/settings.py
@@ -109,6 +109,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_URL'),
         'PORT': os.environ.get('DATABASE_PORT'),
+        
     }
 }
 
