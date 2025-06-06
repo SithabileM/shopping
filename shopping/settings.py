a826801ead659bd28f5afdddaf6bf3bc701f3be8
SithabileM <sithabilemonde9@gmail.com>
2025-06-06 15:47:53 +0200

    make some settings changes

--- shopping/settings.py
+++ shopping/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
+        'HOST': os.environ.get('DATABASE_URL'),
         'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
