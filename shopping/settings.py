189d7b345c55f9767d27e5d889fb7d123abd7c7d
SithabileM <sithabilemonde9@gmail.com>
2025-06-06 15:36:34 +0200

    make some settings changes

--- shopping/settings.py
+++ shopping/settings.py
@@ -104,11 +104,11 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'WoCuAugNJDuUGiSnSCCkUcRebIsvshCF',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
