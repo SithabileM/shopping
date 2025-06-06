5ab81d919bfd2e485d619e3637fb03e1b224b72d
SithabileM <sithabilemonde9@gmail.com>
2025-06-06 16:34:09 +0200

    make some settings changes

--- shopping/settings.py
+++ shopping/settings.py
@@ -104,14 +104,14 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
-        
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WoCuAugNJDuUGiSnSCCkUcRebIsvshCF',
+        'HOST': 'trolley.proxy.rlwy.net',
+        'PORT': '26870',
     }
 }
+
 
 
 # Password validation
