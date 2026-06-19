f40c7ca07bf33408befc4faa04f9f87f20ad987b
SithabileM <sithabilemonde9@gmail.com>
2026-06-19 23:19:31 +0200

    update database url

--- shopping/settings.py
+++ shopping/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")  
 SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "media")
